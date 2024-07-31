a51a7db6346219762e63fcbfc9cfff7e36a0452f
ashutosh sahu <ppaskumar@gmail.com>
2024-07-31 22:49:05 +0530

    Basic Components were Added with static data

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,15 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+
 const App = () => {
   return (
     <center className="todo-container">
-      <h1>TODO APP</h1>
-
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter TODO Here "></input>
-          </div>
-          <div class="col-4">
-            <input type="date"></input>
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">
-              Add
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">Buy Milk</div>
-          <div class="col-4">04/10/2023</div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">Go To College</div>
-          <div class="col-4">04/10/2023</div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
+      <AppName />
+      <AddTodo />
+      <TodoItem1 />
+      <TodoItem2 />
     </center>
   );
 };
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,20 @@
+const AddTodo = () => {
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-6">
+          <input type="text" placeholder="Enter TODO Here "></input>
+        </div>
+        <div class="col-4">
+          <input type="date"></input>
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-success">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AddTodo;
